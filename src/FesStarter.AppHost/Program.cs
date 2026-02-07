5f39b388cda5c9048fd7bb1d296c5643747a46fc
Harry (itsyBOT) <harry@itsybit.se>
2026-02-07 12:51:12 +0100

    Fix cross-platform Angular startup
    - Remove bash-specific PORT syntax from npm start script
    - Use fixed targetPort in Aspire (Angular default 4200)

--- src/FesStarter.AppHost/Program.cs
+++ src/FesStarter.AppHost/Program.cs
@@ -2,10 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.FesStarter_Api>("api");
 
-var web = builder.AddNpmApp("web", "../FesStarter.Web", "start")
+// Angular dev server on default port 4200
+builder.AddNpmApp("web", "../FesStarter.Web", "start")
     .WithReference(api)
     .WaitFor(api)
-    .WithHttpEndpoint(port: 4200, name: "web-http", env: "PORT")
+    .WithHttpEndpoint(targetPort: 4200)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
